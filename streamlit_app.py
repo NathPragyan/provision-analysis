bb32b6aae446b2d5ea0a7a051410d98bc47cc65e
NathPragyan <168570631+NathPragyan@users.noreply.github.com>
2024-10-16 23:47:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 
 # Set page title
 st.title('Interactive Dashboard: Load Trend and Cost Trend Analysis')
+
+# Sidebar to upload multiple files
+st.sidebar.header('Upload your 4 Data Files')
+uploaded_files = st.sidebar.file_uploader("Choose CSV files", accept_multiple_files=True,
