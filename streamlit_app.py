47e56d1d5b7536cbe1e2ffe8416efed6416dffff
NathPragyan <168570631+NathPragyan@users.noreply.github.com>
2024-10-17 00:07:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ st.title('Interactive Dashboard: Load Trend and Cost Trend Analysis')
 # Sidebar to upload multiple files
 st.sidebar.header('Upload your 4 Data Files')
 uploaded_files = st.sidebar.file_uploader("Choose CSV files", accept_multiple_files=True, type=['xlsx'])
+
+ # Sidebar options to choose between Load Trend and Cost Trend
+    trend_option = st.sidebar.selectbox('Choose Trend Type', ['Load Trend', 'Cost Trend'])
