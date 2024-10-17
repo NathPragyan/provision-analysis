e6ca70a4b2baffedbbd34f47b2612f9de18723fb
NathPragyan <168570631+NathPragyan@users.noreply.github.com>
2024-10-17 09:38:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Set page title
 st.title('Provision Analysis: Load Trend and Cost Trend ')
 
 # Sidebar to upload multiple files
 st.sidebar.header('Upload your  Data Files')
-uploaded_files = st.sidebar.file_uploader("Choose CSV files", accept_multiple_files=True, type=['xlsx'])
+uploaded_files = st.sidebar.file_uploader("Choose Provision excel files", accept_multiple_files=True, type=['xlsx'])
+
+# Load all files into a single dataframe
+dataframes = [pd.read_xlsx(file) for file in uploaded_files]
+data = pd.concat(dataframes)
 
  # Sidebar options to choose between Load Trend and Cost Trend
 trend_option = st.sidebar.selectbox('Choose Trend Type', ['Load Trend', 'Cost Trend'])
